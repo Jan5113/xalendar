cfb536915d0e9579638efac48110baabd55e8460
Jan5113 <jan.obermeier@stud.nksa.ch>
2019-04-21 10:07:14 +0200

    init commit

---
+++ XamApp/XamApp/XamApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GetWebTxt(object sender, EventArgs e)
+        {
+            String url = "https://www.obermeier.ch/xalendar/";
+
+            using (var client = new System.Net.WebClient())
+            {
+                var reqparm = new System.Collections.Specialized.NameValueCollection()
+                {
+                    { "msg", "YEETETET" },
+                    { "val", "cunt" },
+                    { "dic", "succ"}
+                };
+
+                client.Headers[System.Net.HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                string responsebody = Encoding.UTF8.GetString(client.UploadValues(url, "POST", reqparm));
+                label.Text = responsebody;
+            }
+        }
+    }
+}
